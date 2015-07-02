4785190251aaf682e2a1c21176d75661fd91c633
Trung Le <trugtuanle@yahoo.com>
2015-07-03 00:33:21 +0700

    Move challenge folder outside of lessons folder

--- client/template/challenge/challenge.js
+++ client/template/challenge/challenge.js
@@ -12,13 +12,15 @@ QTYPE = {
 	LISTEN_VE: 2,
 	MULTIPLE_CHOICES: 3,
 	MULTIPLE_CHOICES_PIC_VE: 4,
-	REARRANGE: 5,
-	FILL_IN_BLANK: 6,
-	MATCHING: 7,
-	REPLACE_WRONG_WORD: 8, 		// Click on wrong word, retype it
-	SELECT_CORRECT_SPLEEING: 9, // 12 phrases flowing through
-	TRANSLATE_EV: 10,
+	REARRANGE: 5,				// Rearrange words in a Vietnamese phrase
+	FILL_IN_BLANK: 6,			// Fill in the blank for a Vietnamese phrase
+	WORD_PAIRING: 7,			// Pairing between English & Vietnamese
+	REPLACE_WRONG_WORD: 8, 		// Use English hint, click on wrong word in Vietnamese phrase
+	SELECT_CORRECT_SPELLING: 9, // Play a tone, and select the correct spelling for the tone
+	TRANSLATE_EV: 10,			// Teach typing in Vietnamese
 	TRUE_FALSE: 11,
+	MULTIPLE_CHOICES_MULTIPLE_ANSWERS: 12, // Multiple choices with multiple answers allowed
+	MULTIPLE_CHOICES_MULTIPLE_ANSWERS_AUDIO: 13, // Multiple choices with multiple answers allowed
 };
 
 // Question state
