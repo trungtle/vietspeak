b32616957c06f0f9c53b89a4cb335fb141f09358
Trung Le <trugtuanle@yahoo.com>
2015-07-18 18:43:41 +0700

    [lesson] - Add FOOD_1 lesson

---
+++ server/lessons/FOODS_1.js
@@ -0,0 +1,102 @@
+FOODS_1 = {
+	name: "Foods 1",
+	level: 2,
+	preview: "Cơm, thịt heo, thịt gà, cá, thịt bò...",
+	phrases: [{
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/rice.jpg",
+		vietnamese: "Cơm",
+		english: ["Rice"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/noodle.jpg",
+		vietnamese: "Mì",
+		english: ["Noodle"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/pork.jpg",
+		vietnamese: "Thịt heo",
+		english: ["Pork"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/beef.jpg",
+		vietnamese: "Thịt bò",
+		english: ["Beef"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/vegetables.jpg",
+		vietnamese: "Rau",
+		english: ["Vegetables"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/she_eats.jpg",
+		vietnamese: "Cô ấy ăn",
+		english: ["She eats"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/she_drinks.jpg",
+		vietnamese: "Cô ấy uống",
+		english: ["She drinks"]
+	}, {
+		qType: QTYPE.WORD_PAIRING,
+	}, {
+		qType: QTYPE.TRANSLATE_VE,
+		vietnamese: "Tôi ăn cơm",
+		english: ["I eat rice"]
+	}, {
+		qType: QTYPE.TRANSLATE_VE,
+		vietnamese: "Anh ấy ăn thịt bò",
+		english: ["He eats beef"]
+	}, {
+		qType: QTYPE.TRANSLATE_VE,
+		vietnamese: "Bạn có thích ăn rau?",
+		english: ["Do you like eating vegetables?"]
+	}, {
+		qType: QTYPE.TRANSLATE_VE,
+		vietnamese: "Cô ấy thích thịt gà",
+		english: ["She likes chicken"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/meat.jpg",
+		vietnamese: "Thịt",
+		english: ["Meat"]
+	}, {
+        qType: QTYPE.REARRANGE,
+    }, {
+		qType: QTYPE.TRANSLATE_VE,
+		vietnamese: "Anh ấy thích cá",
+		english: ["He likes fish"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/bread.jpg",
+		vietnamese: "Bánh mì",
+		english: ["Bread"]
+	}, {
+		qType: QTYPE.WORD_PAIRING,
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/fruits.jpg",
+		vietnamese: "Trái cây",
+		english: ["Fruits"]
+	}, {
+		qType: QTYPE.MULTIPLE_CHOICES_TRANSLATION_PIC,
+		image: "/img/lessons/water.jpg",
+		vietnamese: "Nước",
+		english: ["Water"]
+	}, {
+		qType: QTYPE.TRANSLATE_VE,
+		vietnamese: "Tôi uống nước",
+		english: ["I drink water"]
+	}, {
+		qType: QTYPE.TRANSLATE_VE,
+		vietnamese: "Anh ấy ăn trái cây",
+		english: ["He eats fruits"]
+	}, {
+		qType: QTYPE.TRANSLATE_VE,
+		vietnamese: "Điểm tâm",
+		english: ["Breakfast"]
+	}, {
+		qType: QTYPE.WORD_PAIRING,
+	}]
+};
+
